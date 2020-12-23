5690135cd6b84fd15e12c86b43f7a6eabddaa8cf
TarasDorosh <72886644+TarasDorosh@users.noreply.github.com>
2020-12-23 14:19:47 +0200

    Update views.py

--- Laboratorna3/main/views.py
+++ Laboratorna3/main/views.py
@@ -9,8 +9,5 @@ def main(request):
 
 
 def health(request):
-        response = dict(date= time.strftime("%d %b %Y %H:%M:%S", time.gmtime()),
-                        current_page=request.build_absolute_uri(),
-                        server_info=os.uname().sysname,
-                        client_info=os.getlogin())
+        response = dict(date= time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()), current_page=request.path, server_info=os.name, client_info=os.uname()[1])
         return JsonResponse(response)
